41a0bad8d75c259817e9e8a212c383dc9f537deb
your_user_namer <urvashigolu29@gmail.com>
2024-10-22 22:09:27 +0530

    added Lesson-20

---
+++ Lecture-20/js/script.js
@@ -0,0 +1,41 @@
+//throttling
+throttle = (func, delay) => {
+  let timeoutId;
+
+  return function () {
+    if (!timeoutId) {
+      timeoutId = setTimeout(() => {
+        console.log(this.value);
+        timeoutId = null;
+      }, delay);
+    }
+  };
+};
+
+throttle();
+
+const throttleFun = throttle("apiCall", 2000); //logic, delay
+
+document.querySelector("#name").addEventListener("keyup", throttleFun);
+
+//debouncing
+debounce = (func, delay) => {
+  let timeoutId;
+
+  return function () {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      console.log(this.value);
+    }, delay);
+  };
+};
+
+debounce("hello", 2000);
+
+const debounceFun = throttle("apiCall", 2000); //logic, delay
+
+document.querySelector("#age").addEventListener("keyup", debounceFun);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
