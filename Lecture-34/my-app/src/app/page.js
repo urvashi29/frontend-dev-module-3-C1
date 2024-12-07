2aa3602531700f6edf14ae9b46746cc3bbc63e66
your_user_namer <urvashigolu29@gmail.com>
2024-12-07 21:53:08 +0530

    added Lecture-34

---
+++ Lecture-34/my-app/src/app/page.js
@@ -0,0 +1,10 @@
+"use client";//make your component client is side
+
+//server component
+export default function Home() {
+  console.log('almabetter');
+  
+  return (
+    <p>Hello NextJs</p>
+  )
+}
