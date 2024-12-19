166fbfaf9dd272bfa284ee8213c37697900ea5d8
your_user_namer <urvashigolu29@gmail.com>
2024-12-19 20:15:18 +0530

    changes Employee/index.jsx Lesson-37

--- Lecture-37/my-app/pages/Employee/index.jsx
+++ Lecture-37/my-app/pages/Employee/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "@/components/Button";
+// import Button from "@/components/Button";
 
 // styles-components: allow to add css in js
 
-const Employee = () => {
-  const Button = styled.button`
-    background-color: palevioletred;
-    color: white;
-    padding: 10px;
-    &:hover {
-      background: green;
-    }
-  `;
+const Button = styled.button`
+  background-color: palevioletred;
+  color: white;
+  padding: 10px;
+  &:hover {
+    background: green;
+  }
+`;
 
+function Employee() {
   return (
     <>
       <Button>Hello</Button>
     </>
   );
-};
+}
 
 export default Employee;
